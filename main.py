9f089e415fa62b5e3cd5e3cf47215b665a3d2752
HUANG XUAN KUN <hxk123123123@gmail.com>
2020-01-22 00:34:34 +0800

    update json data src

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ with open('pokedex.json', 'rb') as f:
     # print(data)
 
 output = []
+base_url = "https://raw.githubusercontent.com/HUANGXUANKUN/pokemon-Api/master/images/"
+
 for pokemon in data:
     output_s = {}
     output_s['id'] = pokemon['id']
@@ -20,10 +22,24 @@ for pokemon in data:
     output_s['sp_defense'] = pokemon['base']['Sp. Defense']
     output_s['speed'] = pokemon['base']['Speed']
 
+    url = ""
+    # id_len = len(output_s['id'])
+    id_len = len(str(abs(output_s['id'])))
+
+    if id_len == 1:
+        url += '00' + str(output_s['id']) + '.png'
+    elif id_len == 2:
+        url += '0' + str(output_s['id']) + '.png'
+    else:
+        url += str(output_s['id']) + '.png'
+
+    output_s['url'] = base_url + url
+    print(output_s['url'])
+
     # print("id: {}".format(output_s['id']))
     # print("name: {}".format(output_s['name']))
     # print("type: {}".format(output_s['type']))
     output.append(output_s)
 
-with open('data.json', 'w') as f:
+with open('pokemon_data.json', 'w') as f:
     json.dump(output, f)
